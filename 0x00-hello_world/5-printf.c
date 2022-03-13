a15af7a73ad471aa366446b05443eb627bff67fd
Thugg55 <thuggeru7@gmail.com>
2022-03-13 15:45:24 -0700

    Done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main-comments for the main function
  * Description: Main function also known as the entry point
- * this program prints "with proper grammar, ut the outcome is a piece of art"
+ * this program prints "with proper grammar, but the outcome is a piece of art"
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the otcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
