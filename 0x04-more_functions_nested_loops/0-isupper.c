2c58e04e65e7341f66499f09b2633d8c7b0298ea
Thugg55 <thuggeru7@gmail.com>
2022-03-17 14:19:30 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - output uppercase letter
+ * @c: parameter to include
+ * Return: 1 if uppercase and 0 if otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
