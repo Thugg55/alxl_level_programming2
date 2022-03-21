f0a82ac04056c31036ed81aadd78ea3545077fe4
Thugg55 <thuggeru7@gmail.com>
2022-03-21 14:06:55 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
+
+	return (0);
+}
