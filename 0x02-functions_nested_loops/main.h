76dd585aa5770b238cb9fa7e4fd752271eee2ebd
Thugg55 <thuggeru7@gmail.com>
2022-06-08 11:47:40 -0700

    prototype declaration

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+
 #ifndef main_h
 #define main_h
 /**
@@ -17,5 +18,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_times_table(int n);
 void print_to_98(int n);
 #endif
