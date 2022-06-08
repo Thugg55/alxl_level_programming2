46a7a9a45db537cb00f5bfb4ccfd6efa0896bd49
Thugg55 <thuggeru7@gmail.com>
2022-06-08 12:20:42 -0700

    compute natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Computes all narural numbers below 1024 that are multiples of 3 or 5
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
