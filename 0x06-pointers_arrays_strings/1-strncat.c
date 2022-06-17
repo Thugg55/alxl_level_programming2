2a0f0af8899eebc0d2dd6c9f743e3dea81471da0
Thugg55 <thuggeru7@gmail.com>
2022-06-16 18:09:12 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,20 +14,20 @@ char *_strncat(char *dest, char *src, int n);
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0') /*WHILE count character */
+	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
-
 	j = 0;
-	while (j < n && src[j] != '\0') /*WHILE concatenate dest with src*/
+	while (j < n)
 	{
-		dest[i] = src[j];
-		j++;  /*sum j*/
-		i++; /**sum i*/
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+		{
+			break;
+		}
+		i++;
+		j++;
 	}
-
-	dest[i] = '\0'; /*the terminating null byte*/
-
 	return (dest);
 }
