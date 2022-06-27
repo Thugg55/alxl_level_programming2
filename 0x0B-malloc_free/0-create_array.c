cf126d94cf351f54588d49630ca58064aadbc55f
Thugg55 <thuggeru7@gmail.com>
2022-06-27 11:01:00 -0700

    memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	b = malloc(sizeof(c) * size);
+	b = (int *) malloc(sizeof(c) * size);
 	if (b == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
