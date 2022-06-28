d80ce5f236fe6b411e3a4f39400b8b6043ec23e9
Thugg55 <thuggeru7@gmail.com>
2022-06-28 14:01:34 -0700

    memory allocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,60 +1,30 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- * _strlen - count arrray
- * @s: array of elements
- * Return: i
- */
-
-int _strlen(char *s)
-{
-	unsigned int i;
-	
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * _strcpy - copy arrays
- * @src: array of elements
- * @dest: dest array
- * Return: dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-/**
- * _strdup - array for prints a string
- * @str: array of elements
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string.
  * Return: pointer
  */
 
 char *_strdup(char *str)
 {
-	char *dst;
-	unsigned int str;
+	char *strout;
+	unsigned int i, j;
 
-	if (str == 0)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
-	_strcpy(dst, str);
-	return (dst);
-}
 
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+
+	return (strout);
+}
