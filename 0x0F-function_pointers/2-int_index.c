4bae157ba9fb531f6b00dc9c322348941e134f0e
Thugg55 <thuggeru7@gmail.com>
2022-07-06 21:52:13 -0700

    coreected

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,10 +5,10 @@
  * @array: input integer array
  * @size: size of the array.
  * @cmp: pointer to the function
- * Return: index of the first eement for which the cmp 
- * function does not return 0. If no elementsreturn -1. 
+ * Return: index of the first eement for which the cmp
+ * function does not return 0. If no elementsreturn -1.
  * If size <= 0, return -1.
- */
+ *//
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
