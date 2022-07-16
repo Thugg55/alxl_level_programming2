b8f74a60e565a58d5d78f1f8a386f70d2f5cae4f
Thugg55 <100637930+Thugg55@users.noreply.github.com>
2022-07-16 04:58:04 +0100

    Updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - returns the number of elements in a list_t list.
  * @h: singly linked list
- * Return: Always 0
+ * Return: number of elements in the list
  */
 
 size_t list_len(const list_t *h)
